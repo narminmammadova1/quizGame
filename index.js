e8a59f124f8ee4108ecf5b5618b34ee771ec9563
narminmammadova1 <nerminytb89@gmail.com>
2023-09-11 17:45:44 +0400

    added new functions

---
+++ index.js
@@ -0,0 +1,12 @@
+// const firstDiv=document.querySelector(".firstDiv")
+// const secondDiv=document.querySelector(".secondDiv")
+// const thirdDiv=document.querySelector(".thirdDiv")
+// const start=document.querySelector("#start")
+// const exit=document.querySelector("#start")
+// const card=document.querySelector(".card")
+
+// const cardA=document.querySelector("#A")
+// const cardB=document.querySelector("#B")
+// const cardC=document.querySelector("#C")
+
+
